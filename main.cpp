db3d27118562908070a7e3e22d819e94a38ffa9a
foodims <foodims@union.edu>
2025-02-05 15:00:49 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,23 +1,35 @@
 #include "mbed.h"
 #include "arm_book_lib.h"
 
-int main()
+
+#define DUTY_MIN 0.025
+#define DUTY_MAX 0.125
+#define PERIOD 0.02
+
+#define CONTINUOUS_MIN_STOP .075
+#define CONTINUOUS_FULL_SPEED_CW .065
+#define CONTINUOUS_FULL_SPEED_CCW .085
+#define PERIOD 0.02
+
+PwmOut servo(PE_11);
+PwmOut servo_continuous(PE_9);
+
+int main() 
 {
-    DigitalIn gasDetector(D2);
+    servo.period(PERIOD);
+    servo.write(DUTY_MIN);
 
-    DigitalOut alarmLed(LED1);
-
-    gasDetector.mode(PullDown);
-
-    alarmLed = OFF;
+    servo_continuous.period(PERIOD);
 
     while (true) {
-        if ( gasDetector == ON ) {
-            alarmLed = ON;
-        } 
-        
-        else {
-            alarmLed = OFF;
-        }
+        servo.write(DUTY_MIN);
+        delay(3000);
+        servo.write(DUTY_MAX);
+        delay(3000);
+
+        servo_continuous.write(CONTINUOUS_MIN_STOP);
+
+
+
     }
 }
